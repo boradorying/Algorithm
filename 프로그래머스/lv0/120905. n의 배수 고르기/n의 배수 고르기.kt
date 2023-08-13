2e4f347333ebae51749d56cc140a67732d0473a2
SeJun2 <136980408+boradorying@users.noreply.github.com>
2023-08-13 21:23:28 +0900

    [level 0] Title: n의 배수 고르기, Time: 7.58 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.kt
@@ -0,0 +1,6 @@
+class Solution {
+        fun solution(n: Int, numlist: IntArray): IntArray {
+            var answer: IntArray = numlist.filter{it % n == 0}.toIntArray()
+            return answer
+        }
+    }
